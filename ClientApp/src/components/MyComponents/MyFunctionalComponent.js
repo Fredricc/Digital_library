5a4f41a296d1fd6ec75a2468593baca92936a243
Fredricc <kfred45.fk@gmail.com>
2023-01-19 21:08:26 +0300

    Event Handling

--- ClientApp/src/components/MyComponents/MyFunctionalComponent.js
+++ ClientApp/src/components/MyComponents/MyFunctionalComponent.js
@@ -3,11 +3,12 @@
 const MyFunctionalComponent = (props) => {
 
     function componentMount() {
-        alert("Component rendered");
+        //alert("Component rendered");
     }
     function componentUnmount() {
-        alert("Leaving the component");
+        //alert("Leaving the component");
     }
+
 
     useEffect(() => {
         componentMount();
@@ -19,23 +20,37 @@ const MyFunctionalComponent = (props) => {
     /* STATE */
     const [age, setAge] = useState(20);
 
+    /* EVENT HANDLING */
+    const onChangeAgeInput = (event) => {
+        alert("Age has been changed");
+        setAge(parseInt(event.target.value));
+    }
+    const showDetails = (prTelephone) => {
+        alert(`Name: ${props.name ? props.name : `John`} | Age: ${age} | Telephone: ${prTelephone}`)
+    }
+
     return (
         <div>
-            <h2>
-                My Functional Component!!!
-                { /* Props */}
+            <h2>My Functional Component</h2>
 
-            </h2>
-            <h4> Name: <b> {props.name ? props.name : "Fred"}</b></h4>
-
+            {/* PROPS */}
+            < hr />
+            <h4>Name: <b>{props.name ? props.name : "Fred"}</b></h4>
+             
             {/* STATE */}
-            <hr />
+            < hr />
             <h4>State</h4>
             <span>
-                <b>Age:  </b>{age}
+                <b>Age:  </b><input type= "number" value={age} onChange={onChangeAgeInput} />
                 <button onClick={() => setAge(age + 1)}> + </button>
                 <button onClick={() => setAge(age - 1)}> - </button>
             </span>
+
+            {/* EVENT HANDLING*/}
+            <hr />
+            <h4>Event Handling</h4>
+            <button onClick={showDetails.bind(this, 123456789) }> Show Details</button>
+
         </div>
     )
 }
