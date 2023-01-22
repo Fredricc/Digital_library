840e64a19fa4015ac101db6a2c14ebe62fb3e657
Fredricc <kfred45.fk@gmail.com>
2023-01-22 03:21:42 +0300

    DISPLAY LIBRARIES

--- ClientApp/src/components/MyComponents/LibraryComponent.js
+++ ClientApp/src/components/MyComponents/LibraryComponent.js
@@ -6,14 +6,14 @@ const LibraryComponent = (props) => {
             <hr />
             <h2>Library</h2>
             <div className="row">
-            {/* SEARCH LIBRARY */}
+            {/* SEARCH LIBRARY */ }
             <div className="col-md-4">
                 <div className="card border border-secondary shadow-0">
                     <div className="card-header bg-secondary text-white"><b>Search</b> Library<span className="glyphicon glyphicon-search"></span></div>
                         <div className="card-body">
                             <div className="row">
                                 <div className="col-md-7">
-                                    <label className="form-label">&nbsp;</label>
+                                    <label className="form-label">Name</label>
                                     <input className="form-control" placeholder="Enter Name" name="name" type="text" />
                                 </div>
                                 <div className="col-md-5">
@@ -26,7 +26,35 @@ const LibraryComponent = (props) => {
                         </div>
                     </div>
                 </div>
+                {/* NEW LIBRARY */ }
+                <div className="col-md-8">
+                    <div className="card border border-secondary shadow-0">
+                        <div className="card-header bg-secondary text-white"><b>New</b> Library</div>
+                        <div className="card-body">
+                            <div className="row">
+                                <div className="col-md-3">
+                                    <label className="form-label">Name</label>
+                                    <input className="form-control" placeholder="Enter Name" name="name" type="text"/>
+                                </div>
+                                <div className="col-md-3">
+                                    <label className="form-label">Address</label>
+                                    <input className="form-control" placeholder="Enter Address" name="address" type="text" />
+                                </div>
+                                <div className="col-md-3">
+                                    <label className="form-label">Telephone</label>
+                                    <input className="form-control" placeholder="Enter Telephone" name="Telephone" type="text" />
+                                </div>
+                                <div className="col-md-3">
+                                    <label className="form-label">&nbsp;</label>
+                                    <button type="button" className="btn btn-success form-control">Save</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <br />
+            {/* DISPLAY LIBRARIES*/ }
         </div>
         )
 }
